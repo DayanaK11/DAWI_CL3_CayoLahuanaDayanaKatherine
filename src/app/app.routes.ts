93930fea9ef0b7a14b907e3a4a93e1e4fb0fcc9f
DayanaK11 <dayanacayol@gmail.com>
2024-05-11 13:16:52 -0500

    Modificando detalles del navegador

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -5,7 +5,7 @@ import { Ejercicio3Component } from './ejercicio3/ejercicio3.component';
 import { Ejercicio4Component } from './ejercicio4/ejercicio4.component';
 
 export const routes: Routes = [
-    { path: 'ejercicio1',component : Ejercicio1Component},
+    { path: '',component : Ejercicio1Component},
     { path: 'ejercicio2', component: Ejercicio2Component  },
     { path: 'ejercicio3', component: Ejercicio3Component },
     { path: 'ejercicio4', component:  Ejercicio4Component},
