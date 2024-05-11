2689c5dad8bb0a3d4b987ed31237d7c2b904310c
DayanaK11 <dayanacayol@gmail.com>
2024-05-11 12:45:49 -0500

    Realizando la logica y las funciones del ejercicio03

--- src/app/ejercicio3/ejercicio3.component.ts
+++ src/app/ejercicio3/ejercicio3.component.ts
@@ -9,6 +9,28 @@ import { FormsModule } from '@angular/forms';
   styleUrl: './ejercicio3.component.scss'
 })
 export class Ejercicio3Component {
+  ejercicio3={
+    numero:0,
+  }
+  mensaje1=" Es capicúa ";
+  mensaje2=" No es capicúa ";
+  mensaje="";
+  mostrarMensaje=false;
 
+  calcularCapicua(){
+    
+    const numeroString: string = this.ejercicio3.numero.toString();
+    const primerDigito: string = numeroString.charAt(0);
+    const ultimoDigito: string = numeroString.charAt(1);
+    if(primerDigito==ultimoDigito){
+      this.mensaje=this.mensaje1;
+    }
+    else{
+      this.mensaje=this.mensaje2;
+
+    }
+
+    this.mostrarMensaje=true; 
+  }
 
 }
